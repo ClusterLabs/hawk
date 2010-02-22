3dc43f083b3a5cab95361c8f59469fe915f5de8d
Tim Serong <tserong@novell.com>
2010-02-22 20:26:18 +1100

    Misc: Add note about restarting web server when changing settings.

--- hawk/config/initializers/hawk.rb
+++ hawk/config/initializers/hawk.rb
@@ -1,5 +1,8 @@
 # Global options for HA Web Konsole
-
+#
+# Note that you must restart the web server after changing
+# settings here.
+#
 # To allow free access to management operations, uncomment the
 # following line.  USE THIS WITH EXTREME CAUTION as it will
 # give anyone with a web browser the ability to put nodes on
