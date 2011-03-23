65f38825cdcd8fde7ffbdd470f8865083a608cda
Tim Serong <tserong@novell.com>
2011-03-23 17:27:09 +1100

    GUI: Primitive editor: set default interval if none specified in RA metadata

--- hawk/app/models/primitive.rb
+++ hawk/app/models/primitive.rb
@@ -348,7 +348,14 @@ class Primitive < CibObject
       xml.elements.each('//action') do |e|
         m[:ops][e.attributes['name']] = {}
         ['timeout', 'interval'].each do |a|
-          m[:ops][e.attributes['name']][a.to_sym] = e.attributes[a] if e.attributes[a]
+          if e.attributes[a]
+            m[:ops][e.attributes['name']][a.to_sym] = e.attributes[a]
+          else
+            # There's at least one case (ocf:ocfs2:o2cb) where the
+            # monitor op doesn't specify an interval, so we set a
+            # "reasonable" default
+            m[:ops][e.attributes['name']][a.to_sym] = "20"
+          end
         end
       end
       m
