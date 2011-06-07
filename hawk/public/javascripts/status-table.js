7a9eb36d9cd8b285841285c1604201ae5b688311
Tim Serong <tserong@novell.com>
2011-06-08 02:37:19 +1000

    GUI: table_view: Put clones each in their own row, above primitives

--- hawk/public/javascripts/status-table.js
+++ hawk/public/javascripts/status-table.js
@@ -43,12 +43,18 @@ go by appending to each row.  So there's one HTML table with one row,
 one column per node (plus one for the inactives).  Within each cell we
 just add divs for resources.
 
+That being said, we want clones and ms resources to appear on rows
+by themselves, so you see all clone instances corresponding with each
+other, and indeed can see that they are a clone.  So, instead, we have
+one row for nodes, one row per clone/ms, and one remaining row which
+all regular resources fall into.
+
 */
 var table_view = {
   create: function() {
     var self = this;
-    $("#content").prepend($('<div id="table" style="display: none;"><table><tr style="vertical-align: top;"><td></td></tr></div>'));
-    self.inactive = $("#table").find("td");
+    $("#content").prepend($('<div id="table" style="display: none;"><table cellpadding="0" cellspacing="0"><tbody></tbody></div>'));
+    self.tbody = $("#table").find("tbody");
   },
   destroy: function() {
     // NYI
@@ -58,8 +64,10 @@ var table_view = {
     // Add/update nodes, then add/update resources
     $("#table").show();
     // Temporary shotgun:
-    $("#table").find(".ncol").remove();
-    self.inactive.children().remove();
+    self.tbody.children().remove();
+    self.tbody.append($('<tr class="nrow"></tr><tr></tr>'));
+    var node_row = self.tbody.children(":first");
+    var res_row = self.tbody.children(":last");
     $.each(cib.nodes, function() {
       // Switch cribbed from _cib_to_nodelist_panel()
       var className;
@@ -87,22 +95,30 @@ var table_view = {
           break;
       }
       var d = $(
-        '<td class="ncol"><div id="node::' + this.uname + '" class="ui-corner-all node ns-' + className + '">' +
+        '<td class="ncol" id="ncol::' + this.uname + '"><div id="node::' + this.uname + '" class="ui-corner-all node ns-' + className + '">' +
             '<a id="node::' + this.uname + '::menu"><img src="' + url_root + '/images/transparent-16x16.gif" class="action-icon" alt="" /></a><span id="node::' + this.uname + '::label">' + escape_html(GETTEXT.node_state(this.uname, label)) + '</span>' +
         "</div></td>");
-      self.inactive.before(d);
+      node_row.append(d);
+      res_row.append($('<td class="ncol"></td>'));
       if (!cib_file) {
         add_mgmt_menu($(jq("node::" + this.uname + "::menu")));
       }
     });
     
+    // Inactive column
+    // TODO(must): localize
+    node_row.append($('<td style="text-align: center; vertical-align: middle; color: #888;">Inactive Resources</td>'));
+    res_row.append($("<td></td>"));
+
     $.each(resources_by_id, function() {
       if (!this.instances) return;
       var res_id = this.id;
       $.each(this.instances, function(k) {
         var id = res_id;
+        var is_clone_instance = false;
         if (k != "default") {
           id += ":" + k;
+          is_clone_instance = true;
         }
         // Display logic same as _get_primitive()
         var node = null;
@@ -132,22 +148,39 @@ var table_view = {
           '<div id="resource::' + id + '" class="ui-corner-all ' + status_class + '">' +
               '<a id="resource::' + id + '::menu"><img src="' + url_root + '/images/transparent-16x16.gif" class="action-icon" alt="" /></a><span id="resource::' + id + '::label">' + escape_html(label) + '</span>' +
           "</div>");
-        if (node) {
-          $(jq("node::" + node)).parent().append(d);
+        var append_to = null;
+        if (is_clone_instance) {
+          if ($(jq("crow::" + res_id)).length == 0) {
+            // TODO(must): localize
+//            // TODO(must): will always say clone, even for ms
+//            res_row.before($('<tr><td style="font-size: 80%;">' + GETTEXT.resource_clone(res_id) + "</td></tr>"));
+            res_row.before($('<tr class="crow" id="crow::' + res_id + '"></tr>'));
+            for (var i = 0; i < cib.nodes.length; i++) {
+              $(jq("crow::" + res_id)).append($('<td class="ncol"></td>'));
+            }
+            $(jq("crow::" + res_id)).append($("<td></td>"));
+          }
+          append_to = $(jq("crow::" + res_id));
         } else {
-          self.inactive.append(d);
+          append_to = res_row;
+        }
+        if (node) {
+          // CAN BE ON MULTIPLE NODES (*GAH*)
+          $(append_to.children()[$(jq("ncol::" + node)).index()]).append(d);
+        } else {
+          append_to.children(":last").append(d);
         }
         if (!cib_file) {
           add_mgmt_menu($(jq("resource::" + id + "::menu")));
         }
       });
-    });    
+    });
   },
   hide: function() {
     var self = this;
     $("#table").hide();
+    self.tbody.children().remove();
     $("#table").find(".ncol").remove();
-    self.inactive.children().remove();
   }
 };
 
