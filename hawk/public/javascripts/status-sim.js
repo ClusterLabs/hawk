5eccae7d5900e59b6aede856deba6f96c9063028
Tim Serong <tserong@suse.com>
2011-08-03 00:02:02 +1000

    Misc: simulator: move translatable strings to _gettext.js.erb

--- hawk/public/javascripts/status-sim.js
+++ hawk/public/javascripts/status-sim.js
@@ -35,26 +35,25 @@ var simulator = {
       '<div id="simulator" style="display: none; font-size: 80%;">' +
         "<form><table>" +
           "<tr>" +
-            // TODO(must): Localize, likewise button and link labels
-            "<th>Injected State:</th>" +
+            "<th>" + escape_html(GETTEXT.sim_injected()) + "</th>" +
           "</tr>" +
           "<tr>" +
             '<td><select id="sim-injections" multiple="multiple" size="4" style="width: 15em;"><option></option></select></td>' +
             '<td style="padding-left: 1em;">' +
-              '<button id="sim-run" type="button" style="min-width: 6em;" disabled="disabled">Run &gt;</button> ' +
+              '<button id="sim-run" type="button" style="min-width: 6em;" disabled="disabled">' + escape_html(GETTEXT.sim_run()) + '</button> ' +
             "</td>" +
             '<td style="padding-left: 1em;">' +
-              '<a class="disabled" id="sim-get-info" target="hawk-sim-info">Details</a><br/>' +
-              '<a class="disabled" id="sim-get-in" target="hawk-sim-info">CIB (in)</a><br/>' +
-              '<a class="disabled" id="sim-get-out" target="hawk-sim-info">CIB (out)</a><br/>' +
-              '<a class="disabled" id="sim-get-graph" target="hawk-sim-info">Graph</a>' +
+              '<a class="disabled" id="sim-get-info" target="hawk-sim-info">' + escape_html(GETTEXT.sim_details()) + '</a><br/>' +
+              '<a class="disabled" id="sim-get-in" target="hawk-sim-info">' + escape_html(GETTEXT.sim_cib_in()) + '</a><br/>' +
+              '<a class="disabled" id="sim-get-out" target="hawk-sim-info">' + escape_html(GETTEXT.sim_cib_out()) + '</a><br/>' +
+              '<a class="disabled" id="sim-get-graph" target="hawk-sim-info">' + escape_html(GETTEXT.sim_graph()) + '</a>' +
               ' <a class="disabled" id="sim-get-graph-xml" target="hawk-sim-info">(xml)</a>' +
             "</td>" +
           "</tr>" +
           "<tr>" +
             "<td>" +
-              '<button id="sim-inject-node" type="button" style="min-width: 6em;">+ Node</button> ' +
-              '<button id="sim-inject-op" type="button" style="min-width: 6em;">+ Op</button> ' +
+              '<button id="sim-inject-node" type="button" style="min-width: 6em;">' + escape_html(GETTEXT.sim_inject_node()) + '</button> ' +
+              '<button id="sim-inject-op" type="button" style="min-width: 6em;">' + escape_html(GETTEXT.sim_inject_op()) + '</button> ' +
               '<button id="sim-inject-del" type="button"> - </button> ' +
             "</td>" +
           "</tr>" +
@@ -63,15 +62,14 @@ var simulator = {
 
     $("#sim-inject-node").click(function() {
       var html = "<form><table><tr>" +
-        // TODO(must): localize form labels etc.
-        '<th>Node:</th><td><select id="inject-node-uname">';
+        '<th>' + escape_html(GETTEXT.sim_node_node()) + '</th><td><select id="inject-node-uname">';
       $.each(cib.nodes, function() {
         html += '<option value="' + this.uname + '">' + this.uname + "</option>\n";
       });
-      html += '</select></td><td>&nbsp;</td><th>State:</th><td><select id="inject-node-state">' +
-          '<option value="online">Online</option>' +
-          '<option value="offline">Offline</option>' +
-          '<option value="unclean">Unclean</option>' +
+      html += '</select></td><td>&nbsp;</td><th>' + escape_html(GETTEXT.sim_node_state()) + '</th><td><select id="inject-node-state">' +
+          '<option value="online">' + escape_html(GETTEXT.node_state_online()) + '</option>' +
+          '<option value="offline">' + escape_html(GETTEXT.node_state_offline()) + '</option>' +
+          '<option value="unclean">' + escape_html(GETTEXT.node_state_unclean()) + '</option>' +
           "</select></td>" +
         "</tr></table></form>";
       $("#dialog").html(html);
@@ -85,32 +83,30 @@ var simulator = {
         $(this).dialog("close");
       };
       $("#dialog").dialog("option", {
-        // TODO(must): Localize title
-        title:    "Inject Node State",
+        title:    escape_html(GETTEXT.sim_node_inject()),
         buttons:  b
       });
       $("#dialog").dialog("open");
     });
 
     $("#sim-inject-op").click(function() {
-      // TODO(must): localize form labels etc.
       var html = "<form><table>";
 
-      html += '<tr><th>Operation:</th><td><select id="inject-op-operation">' +
-          '<option value="monitor">Monitor</option>' +
-          '<option value="start">Start</option>' +
-          '<option value="stop">Stop</option>' +
-          '<option value="promote">Promote</option>' +
-          '<option value="demote">Demote</option>' +
-          '<option value="notify">Notify</option>' +
-          '<option value="migrate_to">Migrate To</option>' +
-          '<option value="migrate_from">Migrate From</option>' +
+      html += '<tr><th>' + escape_html(GETTEXT.sim_op_operation()) + '</th><td><select id="inject-op-operation">' +
+          '<option value="monitor">monitor</option>' +
+          '<option value="start">start</option>' +
+          '<option value="stop">stop</option>' +
+          '<option value="promote">promote</option>' +
+          '<option value="demote">demote</option>' +
+          '<option value="notify">notify</option>' +
+          '<option value="migrate_to">migrate_to</option>' +
+          '<option value="migrate_from">migrate_from</option>' +
           "</select></td></tr><tr>";
 
-      html += '<th>Interval:</th>' +
+      html += '<th>' + escape_html(GETTEXT.sim_op_interval()) + '</th>' +
           '<td><input type="text" size="10" id="inject-op-interval"/> (ms)</td></tr>';
 
-      html += '<tr><th>Resource:</td><td><select id="inject-op-resource">';
+      html += '<tr><td>' + escape_html(GETTEXT.sim_op_resource()) + '</td><td><select id="inject-op-resource">';
       $.each(resources_by_id, function() {
         if (!this.instances) return;
         var id = this.id;
@@ -121,20 +117,20 @@ var simulator = {
       });
       html += "</select></td></tr>";
 
-      html += '<tr><th>Result:</th><td><select id="inject-op-result">' +
-          '<option value="success">Success</option>' +
-          '<option value="unknown">Generic Error</option>' +
-          '<option value="args">Wrong Argument(s)</option>' +
-          '<option value="unimplemented">Not Implemented</option>' +
-          '<option value="perm">Permission Denied</option>' +
-          '<option value="installed">Not Installed</option>' +
-          '<option value="configured">Not Configured</option>' +
-          '<option value="not_running">Not Running</option>' +
-          '<option value="master">Running Master</option>' +
-          '<option value="failed_master">Failed Master</option>' +
+      html += '<tr><th>' + escape_html(GETTEXT.sim_op_resource()) + '</th><td><select id="inject-op-result">' +
+          '<option value="success">' + escape_html(GETTEXT.sim_result_success()) + '</option>' +
+          '<option value="unknown">' + escape_html(GETTEXT.sim_result_unknown()) + '</option>' +
+          '<option value="args">' + escape_html(GETTEXT.sim_result_args()) + '</option>' +
+          '<option value="unimplemented">' + escape_html(GETTEXT.sim_result_unimplemented()) + '</option>' +
+          '<option value="perm">' + escape_html(GETTEXT.sim_result_perm()) + '</option>' +
+          '<option value="installed">' + escape_html(GETTEXT.sim_result_installed()) + '</option>' +
+          '<option value="configured">' + escape_html(GETTEXT.sim_result_configured()) + '</option>' +
+          '<option value="not_running">' + escape_html(GETTEXT.sim_result_not_running()) + '</option>' +
+          '<option value="master">' + escape_html(GETTEXT.sim_result_master()) + '</option>' +
+          '<option value="failed_master">' + escape_html(GETTEXT.sim_result_failed_master()) + '</option>' +
           "</select></td></tr>";
 
-      html += '<tr><th>Node:</th><td><select id="inject-op-node">';
+      html += '<tr><th>' + escape_html(GETTEXT.sim_node_node()) + '</th><td><select id="inject-op-node">';
       $.each(cib.nodes, function() {
         html += '<option value="' + this.uname + '">' + this.uname + "</option>\n";
       });
@@ -159,8 +155,7 @@ var simulator = {
         $(this).dialog("close");
       };
       $("#dialog").dialog("option", {
-        // TODO(must): Localize title
-        title:    "Inject Operation",
+        title:    escape_html(GETTEXT.sim_op_inject()),
         buttons:  b
       });
       $("#dialog").dialog("open");
@@ -177,7 +172,7 @@ var simulator = {
     });
 
     $("#sim-run").click(function() {
-      $("#simulator").dialog("option", "title", "Simulator (busy...)");
+      $("#simulator").dialog("option", "title", escape_html(GETTEXT.sim_busy()));
       var i = [];
       $("#sim-injections").children().each(function() {
         i.push($(this).val());
@@ -190,7 +185,7 @@ var simulator = {
         $("#sim-get-out").removeClass("disabled").attr("href", url_root + "/main/sim_get?file=out");
         $("#sim-get-graph").removeClass("disabled").attr("href", url_root + "/main/sim_get?file=graph");
         $("#sim-get-graph-xml").removeClass("disabled").attr("href", url_root + "/main/sim_get?file=graph&format=xml");
-        $("#simulator").dialog("option", "title", "Simulator (final state)");
+        $("#simulator").dialog("option", "title", escape_html(GETTEXT.sim_final()));
       });
       return false;
     });
@@ -220,7 +215,7 @@ var simulator = {
       update_cib();
     };
     $("#simulator").dialog("option", {
-      title:    "Simulator (initial state)",
+      title:    escape_html(GETTEXT.sim_init()),
       buttons:  b
     });
     hide_status();
@@ -231,7 +226,7 @@ var simulator = {
     });
   },
   _reset: function(callback) {
-    $("#simulator").dialog("option", "title", "Simulator (busy...)");
+    $("#simulator").dialog("option", "title", escape_html(GETTEXT.sim_busy()));
     $("#sim-get-info").addClass("disabled").removeAttr("href");
     $("#sim-get-in").addClass("disabled").removeAttr("href");
     $("#sim-get-out").addClass("disabled").removeAttr("href");
@@ -245,7 +240,7 @@ var simulator = {
       if (callback) {
         callback();
       }
-      $("#simulator").dialog("option", "title", "Simulator (initial state)");
+      $("#simulator").dialog("option", "title", escape_html(GETTEXT.sim_init()));
     });
   }
 };
