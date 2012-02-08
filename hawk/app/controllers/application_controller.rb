fe2c8e27fedf2c7dec109852c8c4e51bb29828eb
Tim Serong <tserong@suse.com>
2012-02-09 01:43:41 +1100

    Misc: Suppress ror-sec-scanner warning on filter_parameter_logging

--- hawk/app/controllers/application_controller.rb
+++ hawk/app/controllers/application_controller.rb
@@ -38,7 +38,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery # See ActionController::RequestForgeryProtection for details
 
   # Scrub sensitive parameters from your log
-  filter_parameter_logging :password
+  filter_parameter_logging :password    # RORSCAN_ITL
 
   # Force back to status page if e.g.: cluster offline when trying to access
   # resources, etc.
