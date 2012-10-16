e2feb62954d3904e15208b1973a1c531766e3421
Tim Serong <tserong@suse.com>
2012-10-17 02:40:54 +1100

    Rails3: Update session_store and cookie_verification_secret for rails 3
    Signed-off-by: Tim Serong <tserong@suse.com>

---
+++ hawk/config/initializers/cookie_verification_secret.rb
@@ -0,0 +1,25 @@
+# Be sure to restart your server when you modify this file.
+
+Hawk::Application.config.session_store :cookie_store, key: '_hawk_session'
+
+SESSION_SECRET_FILE = File.join(Rails.root, 'tmp', 'session_secret')
+
+# Your secret key for verifying cookie session data integrity.
+# Uses contents of $RAILS_ROOT/tmp/session_secret.  Creates this
+# file with suitable random contents if it doesn't already exist.
+# Note that ror-sec-scanner picks up secret assignment, but this
+# is OK to ignore.
+Hawk::Application.config.cookie_secret = 
+  if File.exist?(SESSION_SECRET_FILE)  # RORSCAN_ITL
+    File.read(SESSION_SECRET_FILE)
+  else
+    # mkdir tmp here if it doesn't already exist (necessary when
+    # running from a source checkout, wherein tmp does not exist,
+    # which breaks script/generate...)
+    d = File.join(Rails.root, 'tmp')
+    Dir.mkdir d unless File.directory? d
+    secret = SecureRandom.hex(64)
+    File.open(SESSION_SECRET_FILE, 'w', 0600) { |f| f.write(secret) }
+    secret
+  end
+
--- hawk/config/initializers/session_store.rb
+++ hawk/config/initializers/session_store.rb
@@ -2,30 +2,6 @@
 
 Hawk::Application.config.session_store :cookie_store, key: '_hawk_session'
 
-# TODO(rails3): change all this
-SESSION_SECRET_FILE = File.join(RAILS_ROOT, 'tmp', 'session_secret')
-
-# Your secret key for verifying cookie session data integrity.
-# Uses contents of $RAILS_ROOT/tmp/session_secret.  Creates this
-# file with suitable random contents if it doesn't already exist.
-# Note that ror-sec-scanner picks up secret assignment, but this
-# is OK to ignore.
-ActionController::Base.session = {
-  :key         => '_hawk_session',
-  :secret      => if File.exist?(SESSION_SECRET_FILE)  # RORSCAN_ITL
-                    File.read(SESSION_SECRET_FILE)
-                  else
-                    # mkdir tmp here if it doesn't already exist (necessary when
-                    # running from a source checkout, wherein tmp does not exist,
-                    # which breaks script/generate...)
-                    d = File.join(RAILS_ROOT, 'tmp')
-                    Dir.mkdir d unless File.directory? d
-                    secret = ActiveSupport::SecureRandom.hex(64)
-                    File.open(SESSION_SECRET_FILE, 'w', 0600) { |f| f.write(secret) }
-                    secret
-                  end
-}
-
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
 # (create the session table with "rails generate session_migration")
