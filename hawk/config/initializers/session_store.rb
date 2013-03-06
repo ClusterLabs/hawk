964a01e6ee3c15ac83c13c6429e15783fbaa3480
Tim Serong <tserong@suse.com>
2013-03-06 18:19:22 +1100

    Misc: Persist login cookie for 10 days, rather than just browser session
    Signed-off-by: Tim Serong <tserong@suse.com>

--- hawk/config/initializers/session_store.rb
+++ hawk/config/initializers/session_store.rb
@@ -1,6 +1,12 @@
 # Be sure to restart your server when you modify this file.
 
-Hawk::Application.config.session_store :cookie_store, :key => '_hawk_session'
+Hawk::Application.config.session_store :cookie_store, {
+  :key => '_hawk_session',
+  # Allow session cookie to persist for a (somewhat arbitrary) ten days.
+  # This means when using the dashboard you won't be required to log in
+  # to all your clusters all the time.
+  :expire_after => 60 * 60 * 24 * 10
+}
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
