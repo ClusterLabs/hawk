c7f969c6000d67116c730c79180f409c0b94aa98
Thomas Boerger <tboerger@suse.de>
2014-12-17 10:36:05 +0100

    Started to cleanup application assets

--- hawk/app/assets/javascripts/application.js
+++ hawk/app/assets/javascripts/application.js
@@ -29,115 +29,73 @@
 //
 //======================================================================
 
-// Globally useful utility functions only in here
+//= require_tree ./locale
+//= require gettext/all
 
-function jq(id) {
-  return "#" + id.replace(/(:|\.)/g,'\\$1');
-}
-
-function escape_html(str) {
-  return $("<div/>").text(str).html();
-}
-
-function escape_field(s) {
-  return escape_html(s).replace(/"/g, "&quot;").replace(/'/g, "&#39;");
-}
-
-function pad2(n) {
-  return n < 10 ? '0' + n : n;
-}
-
-function date_string(d) {
-  return d.getFullYear() + '-' + pad2(d.getMonth()+1) + '-' + pad2(d.getDate()) + ' ' +
-    pad2(d.getHours()) + ':' + pad2(d.getMinutes()) + ':' + pad2(d.getSeconds());
-}
+//= require_self
 
 $(function() {
-  // Always initialize dialog (it's in the main layout after all...)
-  $("#dialog").dialog({
-    resizable:      false,
-    width:          "30em",
-    draggable:      false,
-    modal:          true,
-    autoOpen:       false,
-    closeOnEscape:  true,
-    close:          function() {
-      if ($(this).dialog("option", "draggable")) {
-        // Reset dialog, but only if it's been changed by the history viewer
-        // (if we do this unconditionally, resetting draggable: false triggers
-        // "uncaught exception: cannot call methods on draggable prior to
-        // initialization; attempted to call method 'destroy'")
-        $(this).dialog("option", {
-          width: "30em",
-          height: "auto",
-          position: "center",
-          resizable: false,
-          draggable: false
-        });
-      } else {
-        // Always reset width (view details on resources sets width auto)
-        $(this).dialog("option", "width", "30em");
+  $('#users #content form')
+    .find('[name="user[roles][]"]')
+      .multiselect({
+        disableIfEmpty: true,
+        enableFiltering: true,
+        buttonWidth: '100%',
+        onChange: function(element, checked) {
+          $(element.context.form).bootstrapValidator(
+            'revalidateField',
+            'user[roles][]'
+          );
+        }
+      })
+      .end()
+    .on('init.form.bv', function(e, data) {
+      $(e.currentTarget).find('[name="revert"]').hide();
+    })
+    .bootstrapValidator({
+      container: 'tooltip',
+      live: 'enabled',
+
+      excluded: ':disabled',
+      submitButtons: 'input[name="submit"]',
+
+      feedbackIcons: {
+        valid: 'fa fa-check',
+        invalid: 'fa fa-times',
+        validating: 'fa fa-refresh'
+      },
+
+      fields: {
+        'user[id]': {
+          message: 'The ID is not valid',
+          validators: {
+            notEmpty: {
+              message: 'The ID is required'
+            }
+          }
+        },
+        'user[roles][]': {
+          message: 'The roles are not valid',
+          validators: {
+            notEmpty: {
+              message: 'The roles are required'
+            }
+          }
+        }
       }
-      var req = $(this).data("req");
-      if (req && req.abort) req.abort();
-    }
-  });
+    })
+    .on('success.field.bv', function(e, data) {
+      $(e.currentTarget)
+        .find('[name="revert"]').end()
+        .find('a.back').attr('data-confirm', __('Any changes will be lost - do you wish to proceed?'));
+
+      return true;
+    })
+    .on('error.field.bv', function(e, data) {
+      $(e.currentTarget)
+        .find('[name="revert"]').end()
+        .find('a.back').attr('data-confirm', __('Any changes will be lost - do you wish to proceed?'));
+
+      return true;
+    });
 });
-
-// Note: /main/gettext?format=js must be included to use this
-function error_dialog(msg, body)
-{
-  if (body) {
-    // TODO(should): theme this properly
-    msg += '<div id="dialog-body" class="message">' + escape_html(body).replace(/\n/g, "<br />") + "</div>";
-  }
-  $("#dialog").html(msg);
-  // TODO(could): Is there a neater construct for this localized button thing?
-  var b = {};
-  b[GETTEXT.ok()]   = function() { $(this).dialog("close"); }
-  $("#dialog").dialog("option", {
-    title:    GETTEXT.error(),
-    buttons:  b
-  });
-  $("#dialog").dialog("open");
-}
-
-// Shame jQuery doesn't seem to give us JSON automatically in the case of an error...
-function json_from_request(request)
-{
-  try {
-    return $.parseJSON(request.responseText);
-  } catch (e) {
-    // This'll happen if the JSON is malformed somehow
-    return null;
-  }
-}
-
-// Rails 2.3.11 fixes a CSRF vulnerability, which requires us to
-// include a CSRF token with every AJAX request.  For details see:
-// http://jasoncodes.com/posts/rails-csrf-vulnerability
-function CSRFProtection(xhr, options) {
-  if (options.cross_domain_hack) {
-    // Cross-domain requests for the cluster dashboard must have *no*
-    // extra headers, or they trigger CORS preflight on Firefox.  Also
-    // we need to ensure session cookies are passed.
-    xhr.withCredentials = true;
-  } else {
-    var token = $('meta[name="csrf-token"]').attr("content");
-    if (token) xhr.setRequestHeader("X-CSRF-Token", token);
-  }
-}
-if ("ajaxPrefilter" in $) {
-  $.ajaxPrefilter(function(options, originalOptions, xhr) { CSRFProtection(xhr, options); });
-} else {
-  $(document).ajaxSend(function(e, xhr, options) { CSRFProtection(xhr, options); });
-}
-
-jQuery.fn.submitWithAjax = function() {
-  this.submit(function() {
-    $.post(this.action, $(this).serialize(), null, "script");
-    return false;
-  })
-  return this;
-};
-
