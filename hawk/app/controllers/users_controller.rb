89f4698585c406fc528ff02e869408571a3d9df6
Thomas Boerger <tboerger@suse.de>
2014-12-17 10:40:44 +0100

    Started to rebuild users module

--- hawk/app/controllers/users_controller.rb
+++ hawk/app/controllers/users_controller.rb
@@ -31,58 +31,140 @@
 
 class UsersController < ApplicationController
   before_filter :login_required
+  before_filter :check_support
+  before_filter :set_title
+  before_filter :set_cib
+  before_filter :set_record, only: [:edit, :update, :destroy]
 
-  layout 'main'
-  before_filter :get_cib
+  def index
+    @users = User.ordered
 
-  def get_cib
-    @cib = Cib.new params[:cib_id], current_user
-  end
-
-  def initialize
-    super
-    @title = _('Edit User')
+    respond_to do |format|
+      format.html
+    end
   end
 
   def new
     @title = _('Create User')
     @user = User.new
+
+    respond_to do |format|
+      format.html
+    end
   end
 
   def create
     @title = _('Create User')
-    unless params[:cancel].blank?
-      redirect_to cib_acls_path
-      return
-    end
     @user = User.new params[:user]
-    if @user.save
-      flash[:highlight] = _('User created successfully')
-      redirect_to :action => 'edit', :id => @user.id
-    else
-      render :action => 'new'
+
+    respond_to do |format|
+      if @user.save
+        format.html do
+          flash[:success] = _('User created successfully')
+          redirect_to edit_cib_user_url(cib_id: @cib.id, id: @user.id)
+        end
+      else
+        format.html do
+          render action: 'new'
+        end
+      end
     end
   end
 
   def edit
-    @user = User.find params[:id]
+    @title = _('Edit User')
+
+    respond_to do |format|
+      format.html
+    end
   end
 
   def update
-    unless params[:revert].blank?
-      redirect_to :action => 'edit'
-      return
+    @title = _('Edit User')
+
+    if params[:revert]
+      return redirect_to edit_cib_user_url(cib_id: @cib.id, id: @user.id)
     end
-    unless params[:cancel].blank?
-      redirect_to cib_acls_path
-      return
+
+    respond_to do |format|
+      if @user.update_attributes(params[:user])
+        format.html do
+          flash[:success] = _('User updated successfully')
+          redirect_to edit_cib_user_url(cib_id: @cib.id, id: @user.id)
+        end
+      else
+        format.html do
+          render action: 'edit'
+        end
+      end
     end
+  end
+
+  def destroy
+    respond_to do |format|
+      if Invoker.instance.crm('--force', 'configure', 'delete', @user.id)
+        format.html do
+          flash[:success] = _('User deleted successfully')
+          redirect_to cib_users_url(cib_id: @cib.id)
+        end
+      else
+        format.html do
+          flash[:alert] = _('Error deleting %s') % @user.id
+          redirect_to cib_users_url(cib_id: @cib.id)
+        end
+      end
+    end
+  end
+
+  protected
+
+  def set_title
+    @title = _('Users')
+  end
+
+  def set_cib
+    @cib = Cib.new params[:cib_id], current_user
+  end
+
+  def set_record
     @user = User.find params[:id]
-    if @user.update_attributes(params[:user])
-      flash[:highlight] = _('User updated successfully')
-      redirect_to :action => 'edit', :id => @user.id
+
+    unless @user
+      respond_to do |format|
+        format.html do
+          flash[:alert] = _('The user does not exist')
+          redirect_to cib_users_url(cib_id: @cib.id)
+        end
+      end
+    end
+  end
+
+  def check_support
+    check = Util.safe_x(
+      '/usr/sbin/cibadmin',
+      '-Ql',
+      '--xpath',
+      '//configuration//crm_config//nvpair[@name=\'enable-acl\' and @value=\'true\']'
+    ).chomp.empty?
+
+    unless check
+      flash.now[:warning] = link_to(
+        _('To enable ACLs, set \'enable-acl\' in the CRM Configuration'),
+        cib_crm_config_path(cib_id: @cib.id)
+      )
+    end
+
+    cibadmin = Util.safe_x(
+      '/usr/sbin/cibadmin',
+      '-Ql',
+      '--xpath',
+      '/cib[@validate-with]'
+    ).lines.first
+
+    if m = cibadmin.match(/validate-with=\"pacemaker-([0-9.]+)\"/)
+      @supported_schema = m.captures[0].to_f >= 2.0
     else
-      render :action => 'edit'
+      @supported_schema = false
     end
   end
 end
