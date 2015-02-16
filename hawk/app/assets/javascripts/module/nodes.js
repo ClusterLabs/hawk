5487ff6e19e374aabbf67334055e7dfed98d4d20
Thomas Boerger <tboerger@suse.de>
2015-02-16 10:24:07 +0100

    Removed content or commented javascripts

--- hawk/app/assets/javascripts/module/nodes.js
+++ hawk/app/assets/javascripts/module/nodes.js
@@ -30,254 +30,254 @@
 //======================================================================
 
 $(function() {
-  $('#dashboards #content table.nodes')
-    .bootstrapTable({
-      method: 'get',
-      url: Routes.cib_nodes_path(
-        $('body').data('cib'),
-        { format: 'json' }
-      ),
-      striped: true,
-      pagination: true,
-      pageSize: 50,
-      pageList: [10, 25, 50, 100, 200],
-      sidePagination: 'client',
-      smartDisplay: false,
-      search: true,
-      searchAlign: 'left',
-      showColumns: true,
-      showRefresh: true,
-      minimumCountColumns: 1,
-      columns: [{
-        field: 'state',
-        title: __('State'),
-        sortable: true,
-        clickToSelect: true,
-        class: 'col-sm-1',
-        formatter: function(value, row, index) {
-          switch(row.state) {
-            case 'online':
-              return [
-                '<i class="fa fa-play" title="',
-                  row.state,
-                '"></i>'
-              ].join('');
-              break;
+  // $('#dashboards #content table.nodes')
+  //   .bootstrapTable({
+  //     method: 'get',
+  //     url: Routes.cib_nodes_path(
+  //       $('body').data('cib'),
+  //       { format: 'json' }
+  //     ),
+  //     striped: true,
+  //     pagination: true,
+  //     pageSize: 50,
+  //     pageList: [10, 25, 50, 100, 200],
+  //     sidePagination: 'client',
+  //     smartDisplay: false,
+  //     search: true,
+  //     searchAlign: 'left',
+  //     showColumns: true,
+  //     showRefresh: true,
+  //     minimumCountColumns: 1,
+  //     columns: [{
+  //       field: 'state',
+  //       title: __('State'),
+  //       sortable: true,
+  //       clickToSelect: true,
+  //       class: 'col-sm-1',
+  //       formatter: function(value, row, index) {
+  //         switch(row.state) {
+  //           case 'online':
+  //             return [
+  //               '<i class="fa fa-play" title="',
+  //                 row.state,
+  //               '"></i>'
+  //             ].join('');
+  //             break;
 
-            default:
-              return [
-                '<i class="fa fa-exclamation-triangle" title="',
-                  row.state,
-                '"></i>'
-              ].join('');
-              break;
-          }
-        }
-      }, {
-        field: 'uname',
-        title: __('Name'),
-        sortable: true,
-        clickToSelect: true
-      }, {
-        field: 'maintenance',
-        title: __('Maintenance'),
-        sortable: true,
-        clickToSelect: true,
-        class: 'col-sm-1',
-        formatter: function(value, row, index) {
-          if (row.maintenance) {
-            return [
-              '<i class="fa fa-toggle-on text-danger" title="',
-                __('Yes'),
-              '"></i>'
-            ].join('');
-          } else {
-            return [
-              '<i class="fa fa-toggle-off text-success" title="',
-                __('No'),
-              '"></i>'
-            ].join('');
-          }
-        }
-      }, {
-        field: 'standby',
-        title: __('Standby'),
-        sortable: true,
-        clickToSelect: true,
-        class: 'col-sm-1',
-        formatter: function(value, row, index) {
-          if (row.standby) {
-            return [
-              '<i class="fa fa-toggle-on text-danger" title="',
-                __('Yes'),
-              '"></i>'
-            ].join('');
-          } else {
-            return [
-              '<i class="fa fa-toggle-off text-success" title="',
-                __('No'),
-              '"></i>'
-            ].join('');
-          }
-        }
-      }, {
-        field: 'operate',
-        title: __('Operations'),
-        sortable: false,
-        clickToSelect: false,
-        class: 'col-sm-1',
-        formatter: function(value, row, index) {
-          var operations = []
+  //           default:
+  //             return [
+  //               '<i class="fa fa-exclamation-triangle" title="',
+  //                 row.state,
+  //               '"></i>'
+  //             ].join('');
+  //             break;
+  //         }
+  //       }
+  //     }, {
+  //       field: 'uname',
+  //       title: __('Name'),
+  //       sortable: true,
+  //       clickToSelect: true
+  //     }, {
+  //       field: 'maintenance',
+  //       title: __('Maintenance'),
+  //       sortable: true,
+  //       clickToSelect: true,
+  //       class: 'col-sm-1',
+  //       formatter: function(value, row, index) {
+  //         if (row.maintenance) {
+  //           return [
+  //             '<i class="fa fa-toggle-on text-danger" title="',
+  //               __('Yes'),
+  //             '"></i>'
+  //           ].join('');
+  //         } else {
+  //           return [
+  //             '<i class="fa fa-toggle-off text-success" title="',
+  //               __('No'),
+  //             '"></i>'
+  //           ].join('');
+  //         }
+  //       }
+  //     }, {
+  //       field: 'standby',
+  //       title: __('Standby'),
+  //       sortable: true,
+  //       clickToSelect: true,
+  //       class: 'col-sm-1',
+  //       formatter: function(value, row, index) {
+  //         if (row.standby) {
+  //           return [
+  //             '<i class="fa fa-toggle-on text-danger" title="',
+  //               __('Yes'),
+  //             '"></i>'
+  //           ].join('');
+  //         } else {
+  //           return [
+  //             '<i class="fa fa-toggle-off text-success" title="',
+  //               __('No'),
+  //             '"></i>'
+  //           ].join('');
+  //         }
+  //       }
+  //     }, {
+  //       field: 'operate',
+  //       title: __('Operations'),
+  //       sortable: false,
+  //       clickToSelect: false,
+  //       class: 'col-sm-1',
+  //       formatter: function(value, row, index) {
+  //         var operations = []
 
-          if (row.state != 'online') {
-            operations.push([
-              '<a href="javascript: void(0);" class="play" title="',
-                __('Online'),
-              '">',
-                '<i class="fa fa-play"></i>',
-              '</a> '
-            ].join(''));
-          }
+  //         if (row.state != 'online') {
+  //           operations.push([
+  //             '<a href="javascript: void(0);" class="play" title="',
+  //               __('Online'),
+  //             '">',
+  //               '<i class="fa fa-play"></i>',
+  //             '</a> '
+  //           ].join(''));
+  //         }
 
-          if (!row.standby) {
-            operations.push([
-              '<a href="javascript: void(0);" class="standby" title="',
-                __('Standby'),
-              '">',
-                '<i class="fa fa-pause"></i>',
-              '</a> '
-            ].join(''));
-          }
+  //         if (!row.standby) {
+  //           operations.push([
+  //             '<a href="javascript: void(0);" class="standby" title="',
+  //               __('Standby'),
+  //             '">',
+  //               '<i class="fa fa-pause"></i>',
+  //             '</a> '
+  //           ].join(''));
+  //         }
 
-          if (!row.maintenance) {
-            operations.push([
-              '<a href="javascript: void(0);" class="maintenance" title="',
-                __('Maintenance'),
-              '">',
-                '<i class="fa fa-wrench"></i>',
-              '</a> '
-            ].join(''));
-          }
+  //         if (!row.maintenance) {
+  //           operations.push([
+  //             '<a href="javascript: void(0);" class="maintenance" title="',
+  //               __('Maintenance'),
+  //             '">',
+  //               '<i class="fa fa-wrench"></i>',
+  //             '</a> '
+  //           ].join(''));
+  //         }
 
-          operations.push([
-            '<a href="javascript: void(0);" class="ready" title="',
-              __('Ready'),
-            '">',
-              '<i class="fa fa-check"></i>',
-            '</a> '
-          ].join(''));
+  //         operations.push([
+  //           '<a href="javascript: void(0);" class="ready" title="',
+  //             __('Ready'),
+  //           '">',
+  //             '<i class="fa fa-check"></i>',
+  //           '</a> '
+  //         ].join(''));
 
-          operations.push([
-            '<a href="javascript: void(0);" class="fence" title="',
-              __('Fence'),
-            '">',
-              '<i class="fa fa-close"></i>',
-            '</a> '
-          ].join(''));
+  //         operations.push([
+  //           '<a href="javascript: void(0);" class="fence" title="',
+  //             __('Fence'),
+  //           '">',
+  //             '<i class="fa fa-close"></i>',
+  //           '</a> '
+  //         ].join(''));
 
-          operations.push([
-            '<a href="javascript: void(0);" class="details" title="',
-              __('Details'),
-            '">',
-              '<i class="fa fa-search"></i>',
-            '</a> '
-          ].join(''));
+  //         operations.push([
+  //           '<a href="javascript: void(0);" class="details" title="',
+  //             __('Details'),
+  //           '">',
+  //             '<i class="fa fa-search"></i>',
+  //           '</a> '
+  //         ].join(''));
 
-          operations.push([
-            '<a href="javascript: void(0);" class="events" title="',
-              __('Events'),
-            '">',
-              '<i class="fa fa-files-o"></i>',
-            '</a>'
-          ].join(''));
+  //         operations.push([
+  //           '<a href="javascript: void(0);" class="events" title="',
+  //             __('Events'),
+  //           '">',
+  //             '<i class="fa fa-files-o"></i>',
+  //           '</a>'
+  //         ].join(''));
 
-          // if (row.state != 'online') {
-          //   operations.push([
-          //     '<li>',
-          //       '<a href="javascript: void(0);" class="play">',
-          //         '<i class="fa fa-play"></i> ',
-          //         __('Online'),
-          //       '</a>',
-          //     '</li>'
-          //   ].join(''));
-          // }
+  //         // if (row.state != 'online') {
+  //         //   operations.push([
+  //         //     '<li>',
+  //         //       '<a href="javascript: void(0);" class="play">',
+  //         //         '<i class="fa fa-play"></i> ',
+  //         //         __('Online'),
+  //         //       '</a>',
+  //         //     '</li>'
+  //         //   ].join(''));
+  //         // }
 
-          // if (!row.standby) {
-          //   operations.push([
-          //     '<li>',
-          //       '<a href="javascript: void(0);" class="standby">',
-          //         '<i class="fa fa-pause"></i> ',
-          //         __('Standby'),
-          //       '</a>',
-          //     '</li>'
-          //   ].join(''));
-          // }
+  //         // if (!row.standby) {
+  //         //   operations.push([
+  //         //     '<li>',
+  //         //       '<a href="javascript: void(0);" class="standby">',
+  //         //         '<i class="fa fa-pause"></i> ',
+  //         //         __('Standby'),
+  //         //       '</a>',
+  //         //     '</li>'
+  //         //   ].join(''));
+  //         // }
 
-          // if (!row.maintenance) {
-          //   operations.push([
-          //     '<li>',
-          //       '<a href="javascript: void(0);" class="maintenance">',
-          //         '<i class="fa fa-wrench"></i> ',
-          //         __('Maintenance'),
-          //       '</a>',
-          //     '</li>'
-          //   ].join(''));
-          // }
+  //         // if (!row.maintenance) {
+  //         //   operations.push([
+  //         //     '<li>',
+  //         //       '<a href="javascript: void(0);" class="maintenance">',
+  //         //         '<i class="fa fa-wrench"></i> ',
+  //         //         __('Maintenance'),
+  //         //       '</a>',
+  //         //     '</li>'
+  //         //   ].join(''));
+  //         // }
 
-          // operations.push([
-          //   '<li>',
-          //     '<a href="javascript: void(0);" class="ready">',
-          //       '<i class="fa fa-check"></i> ',
-          //       __('Ready'),
-          //     '</a>',
-          //   '</li>'
-          // ].join(''));
+  //         // operations.push([
+  //         //   '<li>',
+  //         //     '<a href="javascript: void(0);" class="ready">',
+  //         //       '<i class="fa fa-check"></i> ',
+  //         //       __('Ready'),
+  //         //     '</a>',
+  //         //   '</li>'
+  //         // ].join(''));
 
-          // operations.push([
-          //   '<li>',
-          //     '<a href="javascript: void(0);" class="fence">',
-          //       '<i class="fa fa-close"></i> ',
-          //       __('Fence'),
-          //     '</a>',
-          //   '</li>'
-          // ].join(''));
+  //         // operations.push([
+  //         //   '<li>',
+  //         //     '<a href="javascript: void(0);" class="fence">',
+  //         //       '<i class="fa fa-close"></i> ',
+  //         //       __('Fence'),
+  //         //     '</a>',
+  //         //   '</li>'
+  //         // ].join(''));
 
-          // operations.push([
-          //   '<li class="divider"></li>'
-          // ].join(''));
+  //         // operations.push([
+  //         //   '<li class="divider"></li>'
+  //         // ].join(''));
 
-          // operations.push([
-          //   '<li>',
-          //     '<a href="javascript: void(0);" class="details">',
-          //       '<i class="fa fa-search"></i> ',
-          //       __('Details'),
-          //     '</a>',
-          //   '</li>'
-          // ].join(''));
+  //         // operations.push([
+  //         //   '<li>',
+  //         //     '<a href="javascript: void(0);" class="details">',
+  //         //       '<i class="fa fa-search"></i> ',
+  //         //       __('Details'),
+  //         //     '</a>',
+  //         //   '</li>'
+  //         // ].join(''));
 
-          // operations.push([
-          //   '<li>',
-          //     '<a href="javascript: void(0);" class="events">',
-          //       '<i class="fa fa-files-o"></i> ',
-          //       __('Events'),
-          //     '</a>',
-          //   '</li>'
-          // ].join(''));
+  //         // operations.push([
+  //         //   '<li>',
+  //         //     '<a href="javascript: void(0);" class="events">',
+  //         //       '<i class="fa fa-files-o"></i> ',
+  //         //       __('Events'),
+  //         //     '</a>',
+  //         //   '</li>'
+  //         // ].join(''));
 
-          // return [
-          //   '<div class="btn-group">',
-          //     '<button class="btn btn-default btn-xs dropdown-toggle" type="button" data-toggle="dropdown" aria-expanded="false">',
-          //       __('Operations'),
-          //       ' <span class="caret"></span>',
-          //     '</button>',
-          //     '<ul class="dropdown-menu" role="menu">',
-          //       operations.join(''),
-          //     '</ul>',
-          //   '</div>',
-          // ].join('');
+  //         // return [
+  //         //   '<div class="btn-group">',
+  //         //     '<button class="btn btn-default btn-xs dropdown-toggle" type="button" data-toggle="dropdown" aria-expanded="false">',
+  //         //       __('Operations'),
+  //         //       ' <span class="caret"></span>',
+  //         //     '</button>',
+  //         //     '<ul class="dropdown-menu" role="menu">',
+  //         //       operations.join(''),
+  //         //     '</ul>',
+  //         //   '</div>',
+  //         // ].join('');
 
-          return operations.join('');
-        }
-      }]
-    });
+  //         return operations.join('');
+  //       }
+  //     }]
+  //   });
 });
--- hawk/app/assets/javascripts/module/status.js
+++ hawk/app/assets/javascripts/module/status.js
@@ -32,13 +32,11 @@
 ;(function($, doc, win) {
   'use strict';
 
-  function Status(el, options) {
+  function StatusCheck(el, options) {
     this.$el = $(el);
 
     this.defaults = {
-      requestTimeout: 90000,
-      requestCache: false,
-      currentEpoch: ''
+
     };
 
     this.options = $.extend(
@@ -49,135 +47,16 @@
     this.init();
   }
 
-  Status.prototype.init = function() {
+  StatusCheck.prototype.init = function() {
     var self = this;
 
-    if (self.$el.data('cib') === 'live') {
-      self.process();
 
-      setInterval(
-        self.process,
-        90000
-      );
-    }
-  };
-
-  Status.prototype.process = function() {
-    var self = this;
-
-    $.ajax({
-      url: Routes.monitor_path(),
-      type: 'GET',
-
-      data: self.options.currentEpoch,
-      cache: self.options.requestCache,
-      timeout: self.options.requestTimeout,
-
-      success: function(data) {
-        if (data) {
-          if (data.epoch != self.options.currentEpoch) {
-
-
-
-
-
-
-
-
-            // Trigger full update if the epoch has changed, or if it's a geo-cluster;
-            // this means geo clusters will get an update every minute or so, which
-            // means if it's at all possible for booth status to drift without updating
-            // the CIB, at least it will be apparent within a minute or so.
-            //update_cib();
-
-
-            console.log('update cib');
-
-
-
-
-
-
-
-
-
-
-          } else {
-            self.process();
-          }
-        } else {
-
-
-
-
-
-
-          console.log('no data');
-
-          // This can occur when onSuccess is called in FF on an
-          // aborted request; re-request cib in 15 seconds (see also
-          // beforeunload handler in hawk_init).
-          /*
-          update_errors([GETTEXT.err_conn_aborted()]);
-          hide_status();
-          */
-
-
-
-
-
-
-          setInterval(
-            self.process,
-            15000
-          );
-        }
-      },
-
-      error: function(request) {
-
-
-
-
-
-
-
-        /*
-        if (request.readyState > 1) {
-          // Can't rely on request.status if not ready enough
-          if (request.status >= 10000) {
-            // Crazy winsock(?) error on IE when request aborted
-            update_errors([GETTEXT.err_conn_failed()]);
-          } else {
-            update_errors([GETTEXT.err_unexpected(request.status + " " + request.statusText)]);
-          }
-        } else {
-          // Request timed out
-          update_errors([GETTEXT.err_conn_timeout()]);
-        }
-        hide_status();
-        */
-
-        console.log('error');
-
-
-
-
-
-
-
-        setInterval(
-          self.process,
-          15000
-        );
-      }
-    });
 
   };
 
   $.fn.statusCheck = function(options) {
     return this.each(function() {
-      new Status(this, options);
+      new StatusCheck(this, options);
     });
   };
 }(jQuery, document, window));
