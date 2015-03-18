5c55c43fe0e81f88f3d6284c46f1a727ca6f4880
Kristoffer Grönlund <krig@koru.se>
2015-03-19 00:28:18 +0100

    Rely on hawk to pull in production dependencies

--- chef/cookbooks/hawk/attributes/webui.rb
+++ chef/cookbooks/hawk/attributes/webui.rb
@@ -19,12 +19,12 @@
 
 default["hawk"]["webui"]["packages"] = [
   # Production dependencies
-  "ruby#{node["languages"]["ruby"]["version"].to_f}-rubygem-rails-4_2",
-  "ruby#{node["languages"]["ruby"]["version"].to_f}-rubygem-puma",
-  "ruby#{node["languages"]["ruby"]["version"].to_f}-rubygem-fast_gettext",
-  "ruby#{node["languages"]["ruby"]["version"].to_f}-rubygem-gettext_i18n_rails",
-  "ruby#{node["languages"]["ruby"]["version"].to_f}-rubygem-sprockets",
-  "ruby#{node["languages"]["ruby"]["version"].to_f}-rubygem-tilt-1_4",
+  #"ruby#{node["languages"]["ruby"]["version"].to_f}-rubygem-rails-4_2",
+  #"ruby#{node["languages"]["ruby"]["version"].to_f}-rubygem-puma",
+  #"ruby#{node["languages"]["ruby"]["version"].to_f}-rubygem-fast_gettext",
+  #"ruby#{node["languages"]["ruby"]["version"].to_f}-rubygem-gettext_i18n_rails",
+  #"ruby#{node["languages"]["ruby"]["version"].to_f}-rubygem-sprockets",
+  #"ruby#{node["languages"]["ruby"]["version"].to_f}-rubygem-tilt-1_4",
 
   # Development dependencies
   "ruby#{node["languages"]["ruby"]["version"].to_f}-rubygem-gettext",
