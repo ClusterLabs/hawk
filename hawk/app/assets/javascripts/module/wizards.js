41c5bb3bbed86adc21ec8ce045371831d86a0e24
Kristoffer Grönlund <krig@koru.se>
2015-08-25 11:01:22 +0200

    Wizard: Improved client-side validation for wizards

--- hawk/app/assets/javascripts/module/wizards.js
+++ hawk/app/assets/javascripts/module/wizards.js
@@ -2,27 +2,44 @@
 // See COPYING for license.
 
 $(function() {
-  $('#wizards #middle form')
-    .bootstrapWizard({
-      tabClass: 'nav nav-pills',
-      nextSelector: '.wizard .next',
-      previousSelector: '.wizard .previous',
-      finishSelector: '.wizard .finish',
-      firstSelector: null,
-      lastSelector: null,
-      onNext: function(tab, nav, index) {
-        if ($(nav).parents('form').valid()) {
-          return true;
-        } else {
-          $(nav).parents('form').validate().focusInvalid();
-          return false;
-        }
-      },
-      onTabClick: function(tab, nav, index) {
-        return false;
-      }
-    })
-    .validate({
-      ignore: ":hidden"
+   $('#wizards #middle form')
+        .bootstrapWizard({
+            tabClass: 'nav nav-pills',
+            nextSelector: '.wizard .next',
+            previousSelector: '.wizard .previous',
+            finishSelector: '.wizard .finish',
+            firstSelector: null,
+            lastSelector: null,
+            onNext: function(tab, nav, index) {
+                // Disable validation for all fields that are part
+                // of a non-required step
+                $("[data-depends]").each(function() {
+                    var depelem = document.getElementById($(this).attr('data-depends'));
+                    if (!depelem.checked) {
+                        $(this).rules("remove", "required");
+                        $(this).removeAttr("required");
+                    } else {
+                        $(this).rules("add", "required");
+                    }
+                 });
+                if ($(nav).parents('form').valid()) {
+                    return true;
+                } else {
+                    $(nav).parents('form').validate().focusInvalid();
+                    return false;
+                }
+            },
+            onTabClick: function(tab, nav, index) {
+                return false;
+            }
+        })
+        .validate({
+            ignore: ":hidden"
+        });
+    $("[data-wizard-enable]").change(function() {
+        $('.form-group').each(function () { $(this).removeClass('has-success'); });
+        $('.form-group').each(function () { $(this).removeClass('has-error'); });
+        $('.form-group').each(function () { $(this).removeClass('has-feedback'); });
+        $('.form-control-feedback').each(function () { $(this).remove(); });
     });
 });
