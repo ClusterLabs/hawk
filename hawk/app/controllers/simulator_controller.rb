41712b492a62de1bcf034b350b5c0b3c0c5bc7ab
Kristoffer Grönlund <krig@koru.se>
2015-09-02 20:06:51 +0200

    SimulatorController: Implement intervals

--- hawk/app/controllers/simulator_controller.rb
+++ hawk/app/controllers/simulator_controller.rb
@@ -21,4 +21,30 @@ class SimulatorController < ApplicationController
       end
     end
   end
+
+  # Bit of a hack, used only by simulator to get valid intervals
+  # for the monitor op in milliseconds.  Returns an array of
+  # possible intervals (zero elements if no monitor op defined,
+  # one element in the general case, but should be two for m/s
+  # resources, or more if there's depths etc.).
+  def intervals
+    intervals = []
+    res = Primitive.find params[:id]  # RORSCAN_ITL (authz via cibadmin)
+    Rails.logger.debug "#{res.ops}"
+    res.ops["monitor"].each do |op|
+      Rails.logger.debug "#{params[:id]}, #{op}"
+      intervals << Util.crm_get_msec(op["interval"])
+    end if res.ops.has_key?("monitor")
+    render :json => intervals
+  end
+
+  protected
+
+  def set_title
+    @title = _("Simulator")
+  end
+
+  def set_cib
+    @cib = current_cib
+  end
 end
