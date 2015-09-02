9dfa95ad076b27d550e28e6170657a342d039acc
Kristoffer Grönlund <krig@koru.se>
2015-09-02 20:13:24 +0200

    Simulator: Set resource/operation defaults

--- hawk/app/assets/javascripts/module/simulator.js
+++ hawk/app/assets/javascripts/module/simulator.js
@@ -58,6 +58,47 @@ $(function() {
     self.find("#sim-addop").click(function() {
       var content = $("#modal .modal-content");
       content.html($("#inject-op").render());
+
+      var set_op_defaults = function() {
+        // set sensible default values for the given operation
+        var rsc = content.find("#op-inject-rsc");
+        var op = content.find("#op-inject-op");
+        var interval = content.find("#op-inject-interval");
+
+        interval.prop("disabled", "disabled");
+        interval.val('');
+        if (op.val() == "monitor") {
+          var id = rsc.val().split(":")[0];
+
+          interval.attr("placeholder", "fetching...");
+
+          // get monitor intervals for resource
+          $.getJSON(Routes.sim_intervals_path(id), "", function(data, status, xhr) {
+            if (data.length >= 1) {
+              interval.val(data[0]);
+            }
+            interval.removeProp("disabled").removeAttr("placeholder");
+          });
+        }
+      };
+
+      var set_res_defaults = function() {
+        // This is to select the node the resource is currently running on, in
+        // order that the user inject an op into the "right" place by default.
+        var cib = $("body").data("content");
+        var id_parts = content.find("#op-inject-rsc").val().split(":");
+        var instance = cib.resources_by_id[id_parts[0]].instances[id_parts.length == 2 ? id_parts[1] : "default"];
+        var running_on = instance.master || instance.slave || instance.started || instance.pending;
+        if (running_on) {
+          content.find("#op-inject-node").val(running_on[0].node);
+        }
+        set_op_defaults();
+      };
+
+      set_res_defaults();
+      content.find("#op-inject-rsc").change(set_res_defaults);
+      content.find("#op-inject-op").change(set_op_defaults);
+      content.find("#op-inject-result").val(1);
       content.find("form").submit(function(event) {
         var rsc = content.find("#op-inject-rsc").val();
         var op = content.find("#op-inject-op").val();
