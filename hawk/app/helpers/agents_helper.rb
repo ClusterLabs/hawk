e276fff8053a6377a774360fb7e8466b5b9a0fc2
Kristoffer Grönlund <krig@koru.se>
2015-09-03 03:04:01 +0200

    Fix error handling in AgentsHelper

--- hawk/app/helpers/agents_helper.rb
+++ hawk/app/helpers/agents_helper.rb
@@ -22,35 +22,35 @@ module AgentsHelper
 
   def agent_parameters
     resource_agent = @agent["resource_agent"]
-    return "" unless resource_agent
+    return [] unless resource_agent
     parameters = resource_agent["parameters"]
-    return "" unless parameters
-    return "" unless parameters.is_a? Hash
+    return [] unless parameters
+    return [] unless parameters.is_a? Hash
     parameter = parameters["parameter"]
-    return "" unless parameter
+    return [] unless parameter
     if parameter.is_a? Hash
       [parameter]
     elsif parameter.is_a? Array
       parameter
     else
-      ""
+      []
     end
   end
 
   def agent_actions
     resource_agent = @agent["resource_agent"]
-    return "" unless resource_agent
+    return [] unless resource_agent
     actions = resource_agent["actions"]
-    return "" unless actions
-    return "" unless actions.is_a? Hash
+    return [] unless actions
+    return [] unless actions.is_a? Hash
     action = actions["action"]
-    return "" unless action
+    return [] unless action
     if action.is_a? Hash
       [action]
     elsif action.is_a? Array
       action
     else
-      ""
+      []
     end
   end
 
