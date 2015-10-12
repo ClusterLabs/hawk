e49449388166612f5e99ff543c61e36eb82ee701
Kristoffer Grönlund <krig@koru.se>
2015-10-13 01:13:14 +0200

    Rails complains if we don't skip the CORS after_filter (bsc#950070)

--- hawk/app/controllers/monitor_controller.rb
+++ hawk/app/controllers/monitor_controller.rb
@@ -15,17 +15,20 @@ class MonitorController < ApplicationController
   skip_before_filter :set_shadow_cib
   before_filter :login_required
   skip_before_filter :verify_authenticity_token
+  skip_after_filter :cors_set_access_control_headers
 
   def monitor
     ENV['QUERY_STRING'] = request.query_string.to_s
     ENV['HTTP_ORIGIN'] = request.headers['Origin']
 
     response.headers['Content-Type'] = 'text/event-stream'
-    response.headers['Access-Control-Allow-Origin'] = request.headers["Origin"]
-    response.headers['Access-Control-Allow-Credentials'] = 'true'
-    response.headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, DELETE, OPTIONS'
-    response.headers['Access-Control-Allow-Headers'] = 'Origin, Content-Type, Accept, Authorization, X-CSRF-Token, Token'
-    response.headers['Access-Control-Max-Age'] = "1728000"
+    if request.headers['Origin']
+      response.headers['Access-Control-Allow-Origin'] = request.headers["Origin"]
+      response.headers['Access-Control-Allow-Credentials'] = 'true'
+      response.headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, DELETE, OPTIONS'
+      response.headers['Access-Control-Allow-Headers'] = 'Origin, Content-Type, Accept, Authorization, X-CSRF-Token, Token'
+      response.headers['Access-Control-Max-Age'] = "1728000"
+    end
     Open3.popen2("/usr/sbin/hawk_monitor") do |_i, o, _t|
       result = o.read
       _, body = result.split("\n\n", 2)
