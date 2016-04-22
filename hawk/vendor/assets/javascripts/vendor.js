7f278860d9cf9f27a4e3bddda9fd51b40cca6ed1
Kristoffer Grönlund <krig@koru.se>
2016-04-22 12:51:13 +0200

    dev: Avoid strange asset precompilation issues with js.cookie.js by renaming it to cookie.js

--- hawk/vendor/assets/javascripts/vendor.js
+++ hawk/vendor/assets/javascripts/vendor.js
@@ -15,7 +15,7 @@
 //= require bootstrap
 //= require font-awesome/font-awesome
 //= require misc/highlight
-//= require misc/js.cookie
+//= require misc/cookie
 //= require misc/eventcontrol
 
 //= require js-routes
