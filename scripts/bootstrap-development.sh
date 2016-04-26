b2bfd7f640aa133e2461835f2a4759715ce11574
Kristoffer Grönlund <krig@koru.se>
2016-04-26 11:48:27 +0200

    dev: Simple script to bootstrap a development environment on openSUSE Leap or Tumbleweed

---
+++ scripts/bootstrap-development.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+#
+# Script which tries to bootstrap a Hawk development environment
+
+BASE="$HOME/hawk"
+
+echo "*** Add Virtualization repository"
+if grep 'VERSION="Tumbleweed"' < /etc/os-release >/dev/null 2>&1; then
+	sudo zypper ar http://download.opensuse.org/repositories/Virtualization/openSUSE_Factory/ Virtualization
+elif grep 'Leap' < /etc/os-release >/dev/null 2>&1; then
+	sudo zypper ar http://download.opensuse.org/repositories/Virtualization/openSUSE_Leap_42.1/ Virtualization
+else
+	osver="$(grep 'VERSION=' < /etc/os-release)"
+	echo "Unknown OS version $osver"
+	exit
+fi
+echo "*** zypper refresh"
+sudo zypper refresh
+echo "*** Install development tools"
+sudo zypper install git devel_C_C++ ruby-devel vagrant virtualbox nfs-client nfs-kernel-server
+
+cd "$(dirname "$BASE")" || exit
+
+if [ ! -d "$BASE" ]; then
+	echo "*** Clone hawk repository to $BASE..."
+	git clone --recursive git@github.com:ClusterLabs/hawk "$BASE"
+fi
+
+cd "$BASE" || exit
+
+echo "*** Install vagrant-bindfs plugin"
+if vagrant plugin list | grep bindfs >/dev/null 2>&1; then
+	echo "Already installed."
+else
+	vagrant plugin install vagrant-bindfs || exit
+fi
+
+echo "*** Starting development VM"
+vagrant up webui
