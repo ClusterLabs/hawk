44e2acecb5b6813a03f2e5393108efe136a067bc
Kristoffer Grönlund <krig@koru.se>
2016-06-06 09:36:15 +0200

    UI: Make configuration graph clickable (bsc#983169)

--- hawk/app/helpers/application_helper.rb
+++ hawk/app/helpers/application_helper.rb
@@ -259,4 +259,9 @@ module ApplicationHelper
     graph_path = graph_report_path(id: id, transition: transition, format: "svg", t: Time.now.to_i)
     link_to image_tag(graph_path, class: "img-thumbnail center-block", alt: _("Graph")), graph_path, target: "_blank"
   end
+
+  def config_graph_image(cib_id)
+    graph_path = cib_graph_path(cib_id: cib_id, format: "svg", t: Time.now.to_i)
+    link_to image_tag(graph_path, class: "img-responsive center-block", alt: _("Graph")), graph_path, target: "_blank"
+  end
 end
