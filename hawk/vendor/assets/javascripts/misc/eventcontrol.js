4ab8817586f95ad3cf84d417686e37fda9896446
Kristoffer Grönlund <krig@koru.se>
2016-11-02 16:46:00 +0100

    UI: Fix issues with larger markers (bsc#1001357)

--- hawk/vendor/assets/javascripts/misc/eventcontrol.js
+++ hawk/vendor/assets/javascripts/misc/eventcontrol.js
@@ -3,6 +3,25 @@
 
 (function($) {
   function unit_in_timespan(h, min_time, timespan) {
+    var day = 24*3600*1000;
+    var year = 365*day;
+    if (h > day) {
+      if (timespan > year*4) {
+        var s = moment(min_time).startOf('year');
+        var e = min_time + timespan;
+        var r = [s];
+        var yy = s.year();
+        while (s < e) {
+          yy += 1;
+          s = moment(yy + "-01-01", 'YYYY-MM-DD');
+          r.push(s);
+        }
+        if (r.length > 20) {
+          r = r.slice(0, 20);
+        }
+        return r;
+      }
+    }
     var s = min_time - (min_time % h);
     var e = min_time + timespan;
     if (h > 15*60*1000) {
@@ -67,6 +86,8 @@
     this.labels = this.markers.children('.ec-labels');
     this.min_time = moment("2070-01-01");
     this.max_time = moment("1970-01-01");
+    this.pan_min = this.min_time.clone();
+    this.pan_max = this.max_time.clone();
     this.timespan = MAX_SPAN;
     this.max_timespan = MAX_SPAN;
     this.center_time = this.min_time.valueOf() + MAX_SPAN * 0.5;
@@ -92,6 +113,11 @@
       var time_offset = dragdelta * self.timespan;
       var new_min_time = moment(min_time + time_offset);
       var new_max_time = moment(max_time + time_offset);
+      // disallow pan if panning to where there are no data points
+      if ((new_min_time > self.pan_max && new_min_time > min_time) ||
+          (new_max_time < self.pan_min && new_max_time < max_time)) {
+        return;
+      }
       if (!new_min_time.isSame(self.min_time) || new_max_time.isSame(self.max_time)) {
         self.update_timespan(new_min_time, new_max_time);
       }
@@ -217,6 +243,9 @@
     self.min_time.subtract(5, 's');
     self.max_time.add(5, 's');
     self.center_time = self.min_time.valueOf() + (self.max_time.valueOf() - self.min_time.valueOf()) * 0.5;
+
+    self.pan_min = self.min_time.clone();
+    self.pan_max = self.max_time.clone();
 
     self.update_timespan(self.min_time.clone(), self.max_time.clone());
   };
