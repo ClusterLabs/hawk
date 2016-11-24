3eb8bbb1f7bcac6a1e2908d6d8ca00f741715aac
Ayoub Belarbi <belarbi.ayoub@gmail.com>
2016-11-24 14:34:37 +0100

    UI: Creat ticket - prevent the helpsidebar from disappearing in case of errors

--- hawk/app/controllers/tickets_controller.rb
+++ hawk/app/controllers/tickets_controller.rb
@@ -244,7 +244,7 @@ class TicketsController < ApplicationController
   end
 
   def default_base_layout
-    if ["index", "types", "edit", "new"].include? params[:action]
+    if ["index", "types", "edit", "new", "create"].include? params[:action]
       "withrightbar"
     else
       if params[:action] == "show"
