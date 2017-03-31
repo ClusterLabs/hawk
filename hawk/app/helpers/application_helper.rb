558e8212f473ee1f3689f8c88d9cb0a9a8ccd810
ayoub-belarbi <belarbi.ayoub@gmail.com>
2017-03-31 10:55:25 +0200

    Escape body attrs correctly (bsc#1031822)
    Before wrapping whole value in single quotes, make sure there
    aren't any in the actual value by calling html escape.

--- hawk/app/helpers/application_helper.rb
+++ hawk/app/helpers/application_helper.rb
@@ -57,7 +57,7 @@ module ApplicationHelper
     # e.g: hash_to_attrs({class: "a", data: {cib: "b", epoch: "c"}) will return:
     # "class='a' data-cib='b' data-epoch='c'"
     hash.map{|key, value|
-      value.is_a?(Hash) ? value.map { |k, v| "data-#{k}='#{v}'" } : "#{key}='#{value}'"
+      value.is_a?(Hash) ? value.map { |k, v| "data-#{k}='#{html_escape(v)}'" } : "#{key}='#{html_escape(value)}'"
     }.join(" ").html_safe
   end
 
