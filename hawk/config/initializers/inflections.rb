9fbdda9e22bfc5ce04575fa644973ecfe4fe0c4a
ayoub-belarbi <belarbi.ayoub@gmail.com>
2017-08-30 15:51:43 +0200

    Dev: Restore inflections.rb's content

--- hawk/config/initializers/inflections.rb
+++ hawk/config/initializers/inflections.rb
@@ -1,16 +1,34 @@
-# Be sure to restart your server when you modify this file.
+# Copyright (c) 2009-2015 Tim Serong <tserong@suse.com>
+# See COPYING for license.
 
-# Add new inflection rules using the following format. Inflections
-# are locale specific, and you may define rules for as many different
-# locales as you wish. All of these examples are active by default:
-# ActiveSupport::Inflector.inflections(:en) do |inflect|
-#   inflect.plural /^(ox)$/i, '\1en'
-#   inflect.singular /^(ox)en/i, '\1'
-#   inflect.irregular 'person', 'people'
-#   inflect.uncountable %w( fish sheep )
-# end
+ActiveSupport::Inflector.inflections do |inflect|
+  inflect.uncountable "crm_config"
 
-# These inflection rules are supported but not enabled by default:
-# ActiveSupport::Inflector.inflections(:en) do |inflect|
-#   inflect.acronym 'RESTful'
-# end
+  inflect.acronym "DC"
+  inflect.acronym "SAP"
+  inflect.acronym "NFS"
+  inflect.acronym "STONITH"
+  inflect.acronym "IP"
+  inflect.acronym "ID"
+  inflect.acronym "LVS"
+  inflect.acronym "OCFS2"
+  inflect.acronym "cLVM"
+  inflect.acronym "CIDR"
+  inflect.acronym "SBD"
+  inflect.acronym "QA"
+end
+
+module ActiveSupport::Inflector
+  # does the opposite of humanize ... mostly.
+  # Basically does a space-substituting .underscore
+  def dehumanize(the_string)
+    result = the_string.to_s.dup
+    result.downcase.gsub(/ +/,'_')
+  end
+end
+
+class String
+  def dehumanize
+    ActiveSupport::Inflector.dehumanize(self)
+  end
+end
