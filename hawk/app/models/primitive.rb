3c5348c306fa3b5102e740cd276d3020ef13cd1c
liangxin1300 <XLiang@suse.com>
2017-09-26 13:32:14 +0800

    Dev: Required param's value shouldn't be blank

--- hawk/app/models/primitive.rb
+++ hawk/app/models/primitive.rb
@@ -31,8 +31,17 @@ class Primitive < Template
   validate :validate_params
 
   def validate_params
+    required_params = []
+    res = Hash.from_xml(Util.get_meta_data(agent_name))
+    param_res = res["resource_agent"]["parameters"]["parameter"]
+    param_res.each do |items|
+      if items.key?("required") && items["required"] == "1"
+        required_params << items["name"]
+      end
+    end
+
     params.each do |param, value|
-      if value.blank?
+      if value.blank? && required_params.include?(param)
         errors.add(:base, "In Parameters, #{param}'s value is blank!")
       end
     end
