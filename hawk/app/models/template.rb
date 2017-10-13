b09d67bca58d33f23baeb14b777ad57d8e851183
liangxin1300 <XLiang@suse.com>
2017-10-14 00:23:18 +0800

    Dev: 0 instead of INFINITY means disabled for migration-threshold

--- hawk/app/models/template.rb
+++ hawk/app/models/template.rb
@@ -241,7 +241,7 @@ class Template < Resource
             "migration-threshold" => {
               type: "integer",
               default: "0",
-              longdesc: _("How many failures may occur for this resource on a node, before this node is marked ineligible to host this resource. A value of INFINITY indicates that this feature is disabled.")
+              longdesc: _("How many failures may occur for this resource on a node, before this node is marked ineligible to host this resource. A value of 0 indicates that this feature is disabled.")
             },
             "priority" => {
               type: "integer",
