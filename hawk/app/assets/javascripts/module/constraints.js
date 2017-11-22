f77260554e681c19701410522d402c15fb08085d
ayoub-belarbi <belarbi.ayoub@gmail.com>
2017-11-22 17:22:10 +0100

    Dev: Handle redirection correctly after renaming constraints (bsc#1068942)

--- hawk/app/assets/javascripts/module/constraints.js
+++ hawk/app/assets/javascripts/module/constraints.js
@@ -218,7 +218,7 @@ $(function() {
 
           operations.push([
             '<a href="',
-            Routes.edit_name_cib_constraint_path($('body').data('cib'), row.id),
+            Routes.edit_name_cib_constraint_path($('body').data('cib'), row.id, { source: "edit"}),
             '" class="rename btn btn-default btn-xs" title="',
             __('Rename'),
             '" data-toggle="modal" data-target="#modal">',
