1a2f4db1244628303c77542f48bd41dbdd2a8bec
ayoub-belarbi <belarbi.ayoub@gmail.com>
2017-12-05 12:52:08 +0100

    Test: Add test suit for (bsc#1069296)

---
+++ hawk/spec/lib/util_spec.rb
@@ -0,0 +1,18 @@
+require "rails_helper"
+
+describe Util do
+  describe "#acl_enable?" do
+
+    before do
+      value = true
+      Util.safe_x('/usr/sbin/cibadmin', '--modify', '--xml-text', %Q[<nvpair name="enable-acl" value="#{value}" id="cib-bootstrap-options-enable-acl"/>])
+    end
+
+    context "given that enable-acl property is set to true" do
+      it "returns true" do
+        expect(Util.acl_enabled?).to be true
+      end
+    end
+
+  end
+end
