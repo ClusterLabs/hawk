7c2623228776c232a05d1efa3361edc51142d071
ayoub-belarbi <belarbi.ayoub@gmail.com>
2018-03-19 23:54:54 +0100

    Testing: Add a cluster_env tag
    Skip running tests when the example needs a cluster environment

---
+++ hawk/spec/lib/util_spec.rb
@@ -0,0 +1,20 @@
+require "rails_helper"
+
+unless ENV["TRAVIS"]
+  describe Util, :cluster_env do
+    describe "#acl_enable?" do
+
+      before do
+        value = true
+        Util.safe_x('/usr/sbin/cibadmin', '--modify', '--xml-text', %Q[<nvpair name="enable-acl" value="#{value}" id="cib-bootstrap-options-enable-acl"/>])
+      end
+
+      context "given that enable-acl property is set to true" do
+        it "returns true" do
+          expect(Util.acl_enabled?).to be true
+        end
+      end
+
+    end
+  end
+end
