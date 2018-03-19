ceba31a441d210bdfd0f9162eb18450382ccb023
ayoub-belarbi <belarbi.ayoub@gmail.com>
2018-03-19 22:23:55 +0100

    Api: Create the Api file structure (fate#323437)

---
+++ hawk/app/controllers/api/v1/nodes_controller.rb
@@ -0,0 +1,6 @@
+module Api
+  module V1
+    class NodesController < ApplicationController
+    end
+  end
+end
---
+++ hawk/app/controllers/api/v1/status_controller.rb
@@ -0,0 +1,6 @@
+module Api
+  module V1
+    class StatusController < ApplicationController
+    end
+  end
+end
