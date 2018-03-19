be53400eab3cce8201e701742d910773807e48b1
ayoub-belarbi <belarbi.ayoub@gmail.com>
2018-03-19 22:33:42 +0100

    Api: Create the actions for each controller (fate#323437)

--- hawk/app/controllers/api/v1/nodes_controller.rb
+++ hawk/app/controllers/api/v1/nodes_controller.rb
@@ -1,6 +1,15 @@
 module Api
   module V1
     class NodesController < ApplicationController
+
+      def index
+        render json: { testing_routes: "nodes index"}
+      end
+
+      def show
+        render json: { testing_routes: "nodes show"}
+      end
+
     end
   end
 end
