4fc409c51f6a98923a05f77b03d76d573f8583b9
ayoub-belarbi <belarbi.ayoub@gmail.com>
2018-03-20 22:25:51 +0100

    Api: Implement the structure of http token auth (fate#323437)

--- hawk/app/controllers/api/v1/nodes_controller.rb
+++ hawk/app/controllers/api/v1/nodes_controller.rb
@@ -1,6 +1,6 @@
 module Api
   module V1
-    class NodesController < ActionController::API
+    class NodesController < ApiController
 
       def index
         render json: { testing_routes: "nodes index"}
