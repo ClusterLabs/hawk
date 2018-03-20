1342b367c63e992870bf53f570beba089416cd0e
ayoub-belarbi <belarbi.ayoub@gmail.com>
2018-03-20 22:25:51 +0100

    Api: Implement the structure of http token auth (fate#323437)

--- hawk/app/controllers/api/v1/api_controller.rb
+++ hawk/app/controllers/api/v1/api_controller.rb
@@ -1,6 +1,28 @@
 module Api
   module V1
     class ApiController < ActionController::API
+
+      include ActionController::HttpAuthentication::Token::ControllerMethods
+
+      before_action :authenticate
+
+      protected
+
+        def authenticate
+          authenticate_token || render_unauthorized
+        end
+
+        def authenticate_token
+          authenticate_with_http_token do |token, options|
+            true
+          end
+        end
+
+        def render_unauthorized
+          self.headers["WWW-Authenticate"] = 'Token realm="Application"'
+          render json: 'Bad credentials', status: 401
+        end
+
     end
   end
 end
