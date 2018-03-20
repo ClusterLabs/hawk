c657619690a205eeea968c73e897d43d681f4e97
ayoub-belarbi <belarbi.ayoub@gmail.com>
2018-03-21 00:00:30 +0100

    Testing: Test authentication token

---
+++ hawk/spec/controllers/api/v1/status_controller_spec.rb
@@ -0,0 +1,30 @@
+require "rails_helper"
+
+
+RSpec.describe Api::V1::StatusController do
+
+  context 'without a valid token' do
+    before do
+      @request.headers['Authorization'] = ""
+      get 'index'
+    end
+
+    it 'returns a response with 401 status' do
+      expect(response).to have_http_status 401
+    end
+  end
+
+
+
+  context 'with a valid token' do
+    before do
+      @request.headers['Authorization'] = "Token token_string"
+      get 'index'
+    end
+
+    it 'it returns a response with 200 status code' do
+      expect(response).to have_http_status 200
+    end
+  end
+
+end
