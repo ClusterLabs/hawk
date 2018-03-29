12ef530a5905db6ebf339fbe6b78a05b96c96090
ayoub-belarbi <belarbi.ayoub@gmail.com>
2018-03-29 13:06:40 +0200

    Api: Add registration route (fate#323437)

--- hawk/config/routes.rb
+++ hawk/config/routes.rb
@@ -163,6 +163,7 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
+      post '/register', to: "api#register"
       resources :status, only: :index do
       end
       resources :cluster, only: :index do
