26206353f1d0706987ff00c92412c81deeb8927c
ayoub-belarbi <belarbi.ayoub@gmail.com>
2018-04-24 10:03:15 +0200

    Api: Add models' structure (fate#323437)

---
+++ hawk/app/models/api/v1/api.rb
@@ -0,0 +1,10 @@
+require 'cibtools'
+require 'rexml/document' unless defined? REXML::Document
+require 'rexml/xpath' unless defined? REXML::XPath
+
+module Api
+  module V1
+    class Api
+    end
+  end
+end
