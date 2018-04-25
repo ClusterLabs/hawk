cc5d56d0d7388f0e9701b75c3b58a433ba7826dc
ayoub-belarbi <belarbi.ayoub@gmail.com>
2018-04-25 21:22:41 +0200

    Api: Create models' super class (fate#323437)

--- hawk/app/models/api/v1/node.rb
+++ hawk/app/models/api/v1/node.rb
@@ -1,10 +1,6 @@
-require 'cibtools'
-require 'rexml/document' unless defined? REXML::Document
-require 'rexml/xpath' unless defined? REXML::XPath
-
 module Api
   module V1
-    class Node
+    class Node < Cib
 
       def initialize(root, name)
         @id = name
