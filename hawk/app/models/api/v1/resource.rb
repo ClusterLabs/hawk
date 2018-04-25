3fa50039e0e280571954255e4af65aa228e1c48c
ayoub-belarbi <belarbi.ayoub@gmail.com>
2018-04-25 13:31:17 +0200

    Api: Implement models' logic (fate#323437)

--- hawk/app/models/api/v1/resource.rb
+++ hawk/app/models/api/v1/resource.rb
@@ -1,6 +1,53 @@
 module Api
   module V1
     class Resource < Api
+
+      def initialize(root, id)
+        @id = id
+        @config = REXML::XPath.first(root, "/cib/configuration//primitive[@id='#{id}']")
+        lrm_resources = REXML::XPath.match(root, "/cib/status/node_state/lrm/lrm_resources/lrm_resource[@id='#{id}']")
+        @instances = lrm_resources.map do |lrm_resource|
+          node_state = lrm_resource.parent.parent.parent
+          {
+            node: node_state.attributes["uname"] || node_state.attributes["id"],
+            state: CibTools.determine_resource_state(lrm_resource)
+          }
+        end
+      end
+
+      def id
+        @id
+      end
+
+      def type
+        return :clone if @config.parent.name == "clone"
+        return :multistate if @config.parent.name == "master"
+        :primitive
+      end
+
+      # TODO
+      def state
+        state = :stopped
+        @instances.each do |instance|
+          state = :running if state == :stopped && instance[:state] == :running
+          state = :failed if instance[:state] == :failed
+        end
+        state
+      end
+
+      # TODO
+      def maintenance
+        false
+      end
+
+      def location
+         @instances
+      end
+
+      def to_hash
+        { id: id, type: type, state: state, maintenance: maintenance, location: location }
+      end
+
     end
   end
 end
--- hawk/app/models/api/v1/status.rb
+++ hawk/app/models/api/v1/status.rb
@@ -1,6 +1,70 @@
 module Api
   module V1
     class Status < Api
+
+      def initialize(user="hacluster")
+        @mode = :none
+        @xml = nil
+        cmd = "/usr/sbin/cibadmin"
+        unless File.exists?(cmd)
+          Rails.logger.error "Pacemaker does not appear to be installed (#{cmd} not found)"
+          return
+        end
+        unless File.executable?(cmd)
+          Rails.logger.error "Unable to execute #{cmd}"
+          return
+        end
+        out, err, status = Util.run_as(user, File.basename(cmd), '-Ql')
+        case status.exitstatus
+        when 0
+          @xml = REXML::Document.new(out)
+          unless @xml && @xml.root
+            Rails.logger.error "Failed to parse output of #{cmd} -Ql: #{out}"
+            return
+          end
+        when 54, 13
+          # 13 is cib_permission_denied (used to be 54, before pacemaker 1.1.8)
+          Rails.logger.error "Permission denied for user #{user} calling #{cmd} -Ql"
+          @mode = :offline
+          return
+        else
+          Rails.logger.error "Error invoking #{cmd} -Ql: #{err}"
+          @mode = :offline
+          return
+        end
+        @mode = :online
+      end
+
+      def version
+        if @mode == :online
+          CibTools.epoch_string @xml.root
+        else
+          "0:0:0"
+        end
+      end
+
+      def mode
+        @mode
+      end
+
+      def xml
+        @xml if @mode == :online
+      end
+
+      def nodes
+        return [] if @xml.nil?
+        @xml.elements.collect("/cib/configuration/nodes/node") do |xml|
+          NodeState.new @xml, xml.attributes['uname'] || xml.attributes['id']
+        end
+      end
+
+      def resources
+        return [] if @xml.nil?
+        @xml.elements.collect("/cib/configuration//primitive") do |xml|
+          ResourceState.new @xml, xml.attributes['id']
+        end
+      end
+
     end
   end
 end
