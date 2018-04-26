75bcf2afd6cd769d3bfe7520c01555265e884d19
Lukas Krause <lkrause@suse.de>
2018-04-26 10:56:15 +0200

    Testing: Add rspec fixtures and test api endpoints with valid token

--- hawk/spec/controllers/api/v1/nodes_controller_spec.rb
+++ hawk/spec/controllers/api/v1/nodes_controller_spec.rb
@@ -27,4 +27,19 @@ RSpec.describe Api::V1::NodesController do
     end
   end
 
+
+
+  context 'with a valid token' do
+    before do
+      fake_yaml_store = YAML.load_file(file_fixture("api_token_dummy.store"))
+      allow(File).to receive(:open).and_return(fake_yaml_store)
+      @request.headers['Authorization'] = "Token a123456789"
+      get 'index'
+    end
+
+    it 'it returns a response with 200 status code' do
+      expect(response).to have_http_status 200
+    end
+  end
+
 end
