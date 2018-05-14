34afc6a42bd7090fd353da817dd33af94f680cb6
ayoub-belarbi <belarbi.ayoub@gmail.com>
2018-05-14 09:18:28 +0200

    Dev: Install hawk_monitor binary

--- salt/salt/utils/install_tools.sh
+++ salt/salt/utils/install_tools.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 cd /vagrant || exit
 make WITHIN_VAGRANT=1 WWW_BASE=/vagrant clean
-make WITHIN_VAGRANT=1 WWW_BASE=/vagrant tools/hawk_chkpwd tools/hawk_invoke
+make WITHIN_VAGRANT=1 WWW_BASE=/vagrant tools/hawk_chkpwd tools/hawk_monitor tools/hawk_invoke
 make WITHIN_VAGRANT=1 WWW_BASE=/vagrant tools/install
