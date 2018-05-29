34c5e54bce826b473414e117de246d764b53dda9
Lukas Krause <lkrause@suse.de>
2018-05-29 18:57:36 +0200

    Dev: Prevent error through strip! method on empty score

--- hawk/app/validators/colocation_validator.rb
+++ hawk/app/validators/colocation_validator.rb
@@ -1,21 +1,21 @@
 class ColocationValidator < ActiveModel::Validator
   
   def validate(record)  
-    record.score.strip!
-    unless [
-      "mandatory",
-      "advisory",
-      "inf",
-      "-inf",
-      "infinity",
-      "-infinity"
-    ].include? record.score.downcase
-      unless record.score.match(/^-?[0-9]+$/)
-        record.errors[:score] << _("Invalid score value")
+    unless record.score.blank?
+      record.score.strip!
+      unless [
+        "mandatory",
+        "advisory",
+        "inf",
+        "-inf",
+        "infinity",
+        "-infinity"
+      ].include? record.score.downcase
+        unless record.score.match(/^-?[0-9]+$/)
+          record.errors[:score] << _("Invalid score value")
+        end
       end
-    end
-
-    if record.score.to_s.strip.empty?
+    else
       record.errors[:score] << _("Score is required")
     end
 
