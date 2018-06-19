9d3c080fd5f88ebfe3adb06dca0144be49c25b2c
liangxin1300 <XLiang@suse.com>
2018-06-19 09:53:08 +0800

    Api: Add 'param/meta/op' in resource return value(fate#323437)          To show the parameters/attributes/operations of this ra

--- hawk/app/models/api/v1/resource.rb
+++ hawk/app/models/api/v1/resource.rb
@@ -31,6 +31,22 @@ module Api
         }
       end
 
+      def attributes(root, path)
+        attrs = REXML::XPath.match(root, path)
+        type = File.basename(path)
+        if type == "nvpair"
+          attrs.map do |item|
+            { "#{item.attributes['name']}": item.attributes['value'] }
+          end
+        else
+          attrs.map do |item|
+            item.attributes.each do |key, value|
+              { "#{key}": value }
+            end
+          end
+        end
+      end
+
       # TODO
       def state
         state = :stopped
@@ -62,8 +78,11 @@ module Api
         {
           id: id,
           type: type,
-          script: script,
           state: state,
+          script: script,
+          param: attributes(@config, "instance_attributes/nvpair"),
+          meta: attributes(@config, "meta_attributes/nvpair"),
+          op: attributes(@config, "operations/op"),
           maintenance: maintenance,
           location: location,
           belong: belong
