00caa2580c1752347208071a9c07f31f94409dfe
ayoub-belarbi <belarbi.ayoub@gmail.com>
2018-06-28 16:33:00 +0200

    Dev: Install byebug by default

--- salt/salt/utils/update.sh
+++ salt/salt/utils/update.sh
@@ -5,5 +5,5 @@ zypper ref
 zypper up  -y --replacefiles sbd ruby2.5-rubygem-sass-listen resource-agents   pacemaker ocfs2-tools libqb	 libdlm	 hawk2	 hawk-apiserver	 ha-cluster-bootstrap  fence-agents  drbd-utils	 drbd	 csync2	 crmsh	 corosync
 zypper in -y ruby-devel
 zypper up -y ruby2.5-rubygem-*
-zypper in -y ruby2.5-rubygem-web-console ruby2.5-rubygem-spring ruby2.5-rubygem-uglifier
+zypper in -y ruby2.5-rubygem-web-console ruby2.5-rubygem-byebug ruby2.5-rubygem-spring ruby2.5-rubygem-uglifier
 
