4806cccb10f73443d019b7393bf126b8ad86d529
ayoub-belarbi <belarbi.ayoub@gmail.com>
2018-10-17 12:14:20 +0200

    Dev: Clean up Nagios scripts

--- salt/salt/utils/configure_nagios_client.sh
+++ salt/salt/utils/configure_nagios_client.sh
@@ -1,27 +1,28 @@
 #!/bin/sh
 
-# install the pre-requisite packages.
+# Install the pre-requisite packages.
 zypper --non-interactive install automake autoconf php7 apache2-mod_php7 gd gd-devel lynx w3m perl-Net-SNMP
 
-# Downloading The nagios-plugins Source
+# Download the nagios-plugins Source
 wget --no-check-certificate -O nagios-plugins.tar.gz https://github.com/nagios-plugins/nagios-plugins/archive/release-2.2.1.tar.gz
 tar zxf nagios-plugins.tar.gz
 
+# Install the nagios-plugins
 cd nagios-plugins-release-2.2.1/
-sudo ./tools/setup
-sudo ./configure
-sudo make
-sudo make install
+./tools/setup
+./configure
+make
+make install
 
 # Install nagios-nrpe
 zypper in -y nrpe
 
-# Install Nagios nrpe plugins
+# Install all Nagios nrpe plugins
 zypper in -y 'monitoring-plugins-*'
 
-# Add the webui node (Nagios server), to the list of the allowed hosts
+# Add the webui node (The Nagios server), to the list of the allowed hosts
 sed -i '/^allowed_hosts=/ s/$/,{{ pillar['ip_node_0'] }}/' /etc/nrpe.cfg
 
-# restart the nrpe service
+# Restart the nrpe service
 systemctl restart nrpe
 
--- salt/salt/utils/configure_nagios_server.sh
+++ salt/salt/utils/configure_nagios_server.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-# install the pre-requisite packages.
-zypper --non-interactive install automake autoconf php7 apache2-mod_php7 gd gd-devel lynx w3m libopenssl-devel
+# Install the pre-requisite packages.
+zypper --non-interactive install automake autoconf php7 apache2-mod_php7 gd gd-devel lynx w3m libopenssl-devel perl-Net-SNMP
 
-# Downloading the Source
+# Downloading the Nagios source
 wget -O nagioscore.tar.gz https://github.com/NagiosEnterprises/nagioscore/archive/nagios-4.4.2.tar.gz
 tar xzf nagioscore.tar.gz
 
@@ -16,70 +16,55 @@ make all
 make install-groups-users
 /usr/sbin/usermod -a -G nagios wwwrun
 
-# install the binary files, CGIs, and HTML files.
+# Install the binary files, CGIs, and HTML files.
 make install
 
-# install the service or daemon files and also configures them to start on boot.
+# Install the service or daemon files and also configures them to start on boot.
 make install-daemoninit
 
-# install and configures the external command file.
+# Install and configures the external command file.
 make install-commandmode
 
-# install the *SAMPLE* configuration files. These are required as Nagios needs some configuration files to allow it to start.
+# Install the *SAMPLE* configuration files. These are required as Nagios needs some configuration files to allow it to start.
 make install-config
 
-# install the Apache web server configuration files. Also configure Apache settings if required.
+# Install the Apache web server configuration files. Also configure Apache settings if required.
 make install-webconf
 /usr/sbin/a2enmod rewrite
 /usr/sbin/a2enmod cgi
 /usr/sbin/a2enmod version
 /usr/sbin/a2enmod php7
 
-echo "ServerName localhost" | sudo tee /etc/apache2/conf.d/fqdn.conf
-
+# Fix Apache error: “Could not reliably determine the server's fully qualified domain name”
+echo "ServerName localhost" | tee /etc/apache2/conf.d/fqdn.conf
 systemctl enable apache2.service
 
-# create an Apache user account to be able to log into Nagios. (you will be prompted to provide a password for the account.)
+# Create an Apache user account to be able to log into Nagios. (you will be prompted to provide a password for the account.)
 htpasswd2 -b -c /usr/local/nagios/etc/htpasswd.users nagiosadmin nagios
 systemctl start apache2.service
 systemctl start nagios.service
 
-
-## Plugins
-
-# Installing plugins prerequisites
-zypper in -y perl-Net-SNMP
-
-# Downloading The Source
+# Downloading the nagios plugins Source
 wget --no-check-certificate -O nagios-plugins.tar.gz https://github.com/nagios-plugins/nagios-plugins/archive/release-2.2.1.tar.gz
 tar zxf nagios-plugins.tar.gz
 
-
+# Installing the plugins
 cd nagios-plugins-release-2.2.1/
-sudo ./tools/setup
-sudo ./configure
-sudo make
-sudo make install
-
+./tools/setup
+./configure
+make
+make install
 
 # Change the owner of the /usr/local/nagios directory
 chown -R nagios:nagios /usr/local/nagios
 
-
-
 # Install the nrpe plugins on the server
 zypper in -y monitoring-plugins-nrpe
-sudo cp /usr/lib/nagios/plugins/check_nrpe /usr/local/nagios/libexec
-sudo cp /usr/lib/nagios/plugins/nrpe_check_control /usr/local/nagios/libexec
+cp /usr/lib/nagios/plugins/check_nrpe /usr/local/nagios/libexec
+cp /usr/lib/nagios/plugins/nrpe_check_control /usr/local/nagios/libexec
 
-
-# Edit the localhost.cfg as well to replace the apache2 port
-# Add 8000 to the  /usr/local/nagios/etc/objects/localhost.cfg, and change check_http to check_http_port:
-# Define a service to check HTTP on the local machine.
-# Disable notifications for this service by default, as not all users may have HTTP enabled.
-
+# Change the HTTP port to 8000 in localhost.cfg
 sed -i 's/check_http.*/check_http_port!8000/g' /usr/local/nagios/etc/objects/localhost.cfg
-
 cat << 'EOT' >> /usr/local/nagios/etc/objects/localhost.cfg
 define command {
     command_name check_http_port
@@ -87,11 +72,9 @@ define command {
 }
 EOT
 
-
-
-# add this to /usr/local/nagios/etc/nagios.cfg (see the other jinja file)
+# Add the cluster_api.cfg to nagios config file
 sed -i '/cfg_file=\/usr\/local\/nagios\/etc\/objects\/templates.cfg/a cfg_file=\/usr\/local\/nagios\/etc\/objects\/cluster_api.cfg' /usr/local/nagios/etc/nagios.cfg
 
-# restart the apache2 and nagios services
+# Restart the apache2 and nagios services
 systemctl restart nagios
 systemctl restart apache2
