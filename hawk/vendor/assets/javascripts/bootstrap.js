2d941410aacffbda4b2ef5c36fa0302be4779b34
dmaiocchi <dmaiocchi@suse.com>
2020-06-19 16:20:06 +0200

    update

--- hawk/vendor/assets/javascripts/bootstrap.js
+++
@@ -1,9 +0,0 @@
-// Copyright (c) 2009-2015 Tim Serong <tserong@suse.com>
-// See COPYING for license.
-
-//= require bootstrap/multiselect
-//= require bootstrap/table
-//= require bootstrap/growl
-//= require bootstrap/wizard
-//= require bootstrap/fileinput
-//= require bootstrap/daterangepicker
