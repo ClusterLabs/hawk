f9b76fe55a0589e0a3eb03e57e3fb08cbcba1d80
Aleksei Burlakov <aburlakov@suse.com>
2023-11-14 08:58:04 -0500

    Enforse CSRF in errors_controller.rb
    by changing the protect_from_forgery action to :exception

--- hawk/app/controllers/errors_controller.rb
+++ hawk/app/controllers/errors_controller.rb
@@ -2,7 +2,7 @@
 # See COPYING for license.
 
 class ErrorsController < ActionController::Base
-  protect_from_forgery with: :null_session
+  protect_from_forgery with: :exception
 
   def not_found
     respond_to do |format|
